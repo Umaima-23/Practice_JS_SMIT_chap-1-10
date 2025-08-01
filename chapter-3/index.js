9155136d91be2efe74459a618b98b2f5f58b385d
Umaima-23 <umaimasohail234@gmail.com>
2025-08-01 05:56:47 -0700

    corrected

--- chapter-3/index.js
+++ chapter-3/index.js
@@ -7,7 +7,7 @@ userTrack = "You have visited this site 14 times"
 alert(userTrack);
 // 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser: 
 var birthYear = 2006
-document.write("My birth year is 2006 <br> Data type of my declared varible is number<br><br>")
+document.write("My birth year is" + birthYear +"<br>" + "Data type of my declared varible is number<br><br>")
 /*4. A visitor visits an online clothing store 
 www.xyzClothing.com . Write a script to store in variables 
 the following information: 
@@ -16,7 +16,7 @@ c. Quantity i.e. how many products a visitor wants to
 order 
 Show the following message in your browser: “John 
 Doe ordered 5 T-shirt(s) on XYZ Clothing store”.  */
-var a = "Visitors Name"
-var b = "Product title"
-var c = "Quantity"
-document.write("<strong>John Doe </strong>ordered <strong>5 T-shirt(s)</strong> on XYZ Clothing store")
+var visitorsName = "John Doe"
+var productTitle = "T-shirt(s)"
+var prodQuant = 5
+document.write("<strong>" + visitorsName+ "</strong>ordered <strong>" + prodQuant + productTitle + "</strong> on XYZ Clothing store")
